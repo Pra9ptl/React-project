ab5a31868abd932651ee9ac51917d603fe44fab3
Pranav Patel <patelpranav1313@gmail.com>
2019-09-03 13:37:35 -0400

    useState() hooks

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const App = props => {
+
+  const [personsState, setPersonsState] = useState({
     persons: [
       {name: 'Pranav', age: 22},
       {name: 'Druvin', age: 23},
       {name: 'Rutuj', age: 24}
     ]
-  }
+  });
 
-  changeNameHandler = () => {
+  console.log(personsState);
+
+  const changeNameHandler = () => {
     //console.log('Was Clicked');
-
-    this.setState ({persons: [
+  
+    setPersonsState ({persons: [
       {name: 'Jarvis', age: 22},
       {name: 'Kanudo', age: 23},
       {name: 'Rutuj', age: 30}
     ]});
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>I am JARVIS</h1>
-        <button onClick={this.changeNameHandler}>Switch Name</button>
-        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age}/>
-        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}> My Hobbies: Coding</Person>
-        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age}/>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>I am JARVIS</h1>
+      <button onClick={changeNameHandler}>Switch Name</button>
+      <Person name = {personsState.persons[0].name} age = {personsState.persons[0].age}/>
+      <Person name = {personsState.persons[1].name} age = {personsState.persons[1].age}> My Hobbies: Coding</Person>
+      <Person name = {personsState.persons[2].name} age = {personsState.persons[2].age}/>
+    </div>
+  );
 }
 
 export default App;
+

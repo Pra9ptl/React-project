63af5dcdc35828e967d9dcbc82b64ed18d9dd81f
Pra9ptl <patelpranav1313@gmail.com>
2020-04-01 14:54:17 -0400

    applying media query using modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
+import classes from './Person.css'
 
-const StyledDiv = styled.div`
-    width: 80%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-    @media (min-width: 700px){
-        width: 450px;
-     }
-`;
 const Person = (props) => {
 
     // const styles = {
@@ -22,11 +11,11 @@ const Person = (props) => {
 
     return (
         //<div className="Person" style={styles}>
-        <StyledDiv>
+        <div className={classes.Person}>
             < p onClick={props.click} > I'm {props.name}. My age is {props.age}.</p>
             < p > {props.children}</p >
             <input type="text" onChange={props.change} value={props.name} />
-        </StyledDiv>
+        </div>
     )
 };
 

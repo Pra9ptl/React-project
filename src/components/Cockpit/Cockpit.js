e2f42fe4ac557b6bfba30a99573a8288b86b45b9
Pra9ptl <patelpranav1313@gmail.com>
2020-04-03 14:50:23 -0400

    Use of useEffect() in functional component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 
 const cockpit = (props) => {
+
+    //Can use as many useEffect...
+    //this useEffect will run initially and when persons is changed or also can pass multiple fields which it can depend on
+    // useEffect(() => {
+    //     console.log('[Cockpit.js] useEffect');
+    //     //https requests...
+    //     setTimeout(() => {
+    //         alert('Saved date to cloud');
+    //     }, 1000);
+    // }, [props.persons]);
+
+    //this useEffect will run only for the first time as it  has no depedency change
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect');
+        //https requests...
+        setTimeout(() => {
+            alert('Saved date to cloud');
+        }, 1000);
+    }, []);
 
     const assignedClasses = [];
     let btnClass = '';

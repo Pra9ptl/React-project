faed3aebef6d255be124d903bbc72cd7b477d140
Pra9ptl <patelpranav1313@gmail.com>
2020-04-05 14:51:17 -0400

    Code optimization in funcation components.

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -42,10 +42,10 @@ const cockpit = (props) => {
     if(props.showPerson){
         btnClass = classes.Red;
     }
-    if (props.persons.length <= 2) {
+    if (props.personsLength <= 2) {
       assignedClasses.push(classes.red);
     }
-    if (props.persons.length <= 1) {
+    if (props.personsLength <= 1) {
       assignedClasses.push(classes.bold);
     }
 
@@ -59,4 +59,4 @@ const cockpit = (props) => {
     );
 };
 
-export default cockpit;
+export default React.memo(cockpit);
